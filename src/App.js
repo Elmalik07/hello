27f51b2a9b3e399550e6cf433571e6fa82ef8df0
Elmalik07 <malik.tiemogo@gmail.com>
2022-08-01 15:07:33 +0000

    testngor

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello React
+          Hello Ngor !
         </a>
       </header>
     </div>
