01eff0f21beb5b57d99e80b8e29d619607f1d649
Elmalik07 <malik.tiemogo@gmail.com>
2022-08-02 20:57:57 +0000

    maj App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Jenkins!
+          Hello Supdeco!
         </a>
       </header>
     </div>
